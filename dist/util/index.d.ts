6a8d3d2b2c272353ca076ca17bf03b20639ce40d
Paul Wand <Paul.Wand@nrel.gov>
2021-01-26 11:31:55 -0700

    export AppConfig

--- dist/util/index.d.ts
+++ dist/util/index.d.ts
@@ -1,4 +1,6 @@
+import { AppConfig } from "./AppConfig";
 import remove from "./remove";
 import titleCase from "./titleCase";
 import unique from "./unique";
 export { remove, titleCase, unique };
+export type { AppConfig };
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,8 @@
+import { AppConfig } from "./AppConfig";
 import remove from "./remove";
 import titleCase from "./titleCase";
 import unique from "./unique";
 
-export { remove, titleCase, unique } 
+export { remove, titleCase, unique };
+export type { AppConfig };
+
