272e95f483f7a29bce6edb5612bff53ba10f6cc6
Paul Wand <Paul.Wand@nrel.gov>
2021-02-12 12:10:41 -0700

    save files as binary strings, behave well with AppForm

--- src/components/serialization/AppUploader.tsx
+++ src/components/serialization/AppUploader.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Dropzone, { IFileWithMeta, IUploadParams, StatusValue } from 'react-dropzone-uploader';
+import Dropzone, { IFileWithMeta, IMeta, IUploadParams, StatusValue } from 'react-dropzone-uploader';
 import 'react-dropzone-uploader/dist/styles.css';
-import { AppCard } from '..';
+import { AppItem, AppLabel } from '..';
 import { AppColor } from '../../theme';
-import { useFileStorage } from '../../hooks';
-import { v4 } from 'uuid';
+import { prettyTitle } from '../../util';
+import AppText from '../AppText';
 
 export type AppFill = "clear" | "outline" | "solid" | "default" | undefined
 
@@ -13,49 +13,58 @@ export interface uploaderProps {
     title: string,
     description: string,
     file?: File
-    onFileReceived: (file: IFileWithMeta, uri: string) => void
+    onFileReceived: (meta: IMeta, data_uri: string) => void
     accept: string,
-    identifier: string
     uploadParams?: (fileWithMeta: IFileWithMeta) => IUploadParams
 }
 
 /**
  * Upload Component 
  */
-const AppUploader: React.FC<uploaderProps> = ({ accept, description, title, children, onFileReceived, uploadParams, identifier, file }) => {
+const AppUploader: React.FC<uploaderProps> = ({ accept, description, title, onFileReceived, uploadParams, file }) => {
     const [status, setStatus] = useState<StatusValue>("ready");
     const successStatus: StatusValue[] = ["done"];
     const errorStatus: StatusValue[] = ["error_file_size", "error_upload", "error_upload_params", "error_validation", "aborted", "rejected_file_type"]
     const normalStatus: StatusValue[] = ["ready", "preparing", "getting_upload_params"]
     const statusColor: AppColor = normalStatus.includes(status) ? "primary" : successStatus.includes(status) ? "success" : errorStatus.includes(status) ? "danger" : "clear";
 
-    const { insert } = useFileStorage();
+    const propertyFormattedName = prettyTitle(title);
 
     // called every time a file's `status` changes
-    const handleChangeStatus: (meta: IFileWithMeta, status: StatusValue, allFiles: IFileWithMeta[]) => void = ({ meta, file }, status) => {
+    const handleChangeStatus: (fileWithMeta: IFileWithMeta, status: StatusValue, allFiles: IFileWithMeta[]) => void = (fileWithMeta, status) => {
+        if (status === "done") {
+            handleSubmit(fileWithMeta);
+        }
         setStatus(status);
     }
 
-    const handleSubmit: (successFiles: IFileWithMeta[], allFiles: IFileWithMeta[]) => void = (files, allFiles) => {
-        allFiles.forEach((fileWithMeta) => {
-            const { file } = fileWithMeta;
-            const URI = "/" + identifier + v4();
-            insert(file, URI);
-            onFileReceived(fileWithMeta, URI);
-        })
+    const handleSubmit: (successFile: IFileWithMeta) => Promise<void> = async (fileWithMeta) => {
+        const { meta, file } = fileWithMeta;
+        const fileBuffer = await file.arrayBuffer()
+        var binary = '';
+        var bytes = new Uint8Array(fileBuffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        onFileReceived(meta, btoa(binary));
     }
     return <>
-        <AppCard titleColor={statusColor} title={title} subTitle={description}>
-
-            <Dropzone
-                initialFiles={file ? [file] : undefined}
-                maxFiles={1}
-                multiple={false}
-                getUploadParams={uploadParams}
-                onChangeStatus={handleChangeStatus}
-                onSubmit={handleSubmit}
-                accept={accept}
-            />        </AppCard>
+        <AppItem color="clear" lines="none">
+            <AppLabel position="stacked" color={statusColor} >
+                {propertyFormattedName}
+            </AppLabel>
+        </AppItem>
+        <AppText>{description}</AppText>
+        <Dropzone
+            initialFiles={file ? [file] : undefined}
+            maxFiles={1}
+            multiple={false}
+            getUploadParams={uploadParams}
+            autoUpload={true}
+            onChangeStatus={handleChangeStatus}
+            accept={accept}
+        />
     </>
 };
 
