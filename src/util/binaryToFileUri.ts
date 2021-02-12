d871efdb83bde9d048c592ebdab1a51b0e5041c3
Paul Wand <Paul.Wand@nrel.gov>
2021-02-12 12:09:27 -0700

    convert binary to object URI helper

---
+++ src/util/binaryToFileUri.ts
@@ -0,0 +1,7 @@
+export const binaryToFileUri = (binary: string, type: string) => {
+    var bytes = new Uint8Array(binary.length);
+    for (var i = 0; i < binary.length; i++)
+        bytes[i] = binary.charCodeAt(i);
+    const file = new File([bytes], "name", { type });
+    return URL.createObjectURL(file);
+}
