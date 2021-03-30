041b220c1df96e8a346a8db943322fc9c483dd57
Paul Wand <Paul.Wand@nrel.gov>
2021-03-30 09:56:30 -0600

    improve pretty title func

--- src/util/prettyTitle.ts
+++ src/util/prettyTitle.ts
@@ -2,5 +2,5 @@
  * Make the first char in a string caps
  * @param str string to Uppercase
  */
-const prettyTitle = (str: string = "") => ((str).charAt(0).toUpperCase() + str.slice(1)).split("_").join(" ");
+const prettyTitle = (str: string = "") => ((str).charAt(0).toUpperCase() + str.slice(1)).split("_").join(" ").split("-").join(" ");
 export default prettyTitle;
