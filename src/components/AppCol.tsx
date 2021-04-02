5fa7fc2b5caebe2551eedc4a4b12a244698ff6ae
Paul Wand <Paul.Wand@nrel.gov>
2021-04-02 12:35:28 -0600

    double amount of columns

--- src/components/AppCol.tsx
+++ src/components/AppCol.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { AppColor } from '../theme/AppColor';
 
 
+export type columnAmount = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12" |
+    "13" | "14" | "15" | "16" | "17" | "18" | "19" | "20" | "21" | "22"| "23"| "24"
+
 interface itemProps {
-    size?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12"
-    sizeMd?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12"
-    sizeLg?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12"
-    sizeXs?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "11" | "12"
+    size?: columnAmount
+    sizeMd?: columnAmount
+    sizeLg?: columnAmount
+    sizeXs?: columnAmount
     color?: AppColor
 }
 
