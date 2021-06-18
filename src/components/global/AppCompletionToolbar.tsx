a76bac5c4e2f717c11f0ef3896aeae3a5eb14093
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-06-18 17:14:05 -0600

    handle server connection state

--- src/components/global/AppCompletionToolbar.tsx
+++ src/components/global/AppCompletionToolbar.tsx
@@ -1,8 +1,9 @@
+import axios from 'axios';
 import { settingsOutline } from 'ionicons/icons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppButtons, AppChip, AppInput, AppModal, AppToggle, AppToolbar } from '..';
 import { useAppLayout } from '../../hooks';
-import { useAppSettings } from '../../hooks/useAppSettings';
+import { ServerStatus, useAppSettings } from '../../hooks/useAppSettings';
 import useCompletion from '../../hooks/useCompletion';
 import { AppBottomBarConfig, AppCompletionConfig } from '../../util/AppConfig';
 import AppButton from '../AppButton';
@@ -18,8 +19,22 @@ export const AppCompletionProgress: React.FC = () => {
 }
 export const AppSettingsModal: React.FC = () => {
     const [showSettings, setShowSettings] = useState(false);
-    const { darkMode, setDarkMode, setServer, server } = useAppSettings();
+    const { darkMode, setDarkMode, server, setServer, authorized, setAuthorized, serverStatus, setServerStatus } = useAppSettings();
     const { title } = useAppLayout();
+    const [tempServer, setTempServer] = useState<string>(server || "")
+    useEffect(() => {
+
+        if (authorized && server && serverStatus === "connecting") {
+            axios.get(server).then(() => {
+                setServerStatus("connected")
+            }).catch(() => {
+                setServerStatus("error")
+            })
+        } else if (!authorized && server) {
+            const authorized = window.confirm("To connect to a server will open a connection to the specified URI.\n Are you in a trusted environment?\n Are you sure you want to do this?")
+            setAuthorized(authorized);
+        }
+    }, [authorized, server, serverStatus, setAuthorized, setServerStatus])
     return showSettings ? <AppModal isOpen={showSettings} onDismiss={() => { setShowSettings(false) }}>
         < AppCard title={title + " Settings"} headerColor="tertiary" >
             <AppItem>
@@ -34,16 +49,21 @@ export const AppSettingsModal: React.FC = () => {
                 <AppChip>
                     Atomic Server URI:
                 </AppChip>
-                <AppInput value={server} placeholder={"https://atomic-server-uri:{port-number}"} onInputChange={(input) => {
-                    setServer(input)
+                <AppInput value={tempServer} placeholder={"https://atomic-server-uri:{port-number}"} onInputChange={(input) => {
+                    setTempServer(input)
                 }} >
                 </AppInput>
+                <AppButtons slot="end">
+                    {server && <AppChip color={serverStatus === "connected" ? "favorite" : serverStatus === "connecting" ? "medium" : "danger"}>{serverStatus}</AppChip>}
+                </AppButtons>
             </AppItem>
-            {server && <AppButton onClick={() => {
-
+            {server && tempServer !== server && <AppButton onClick={() => {
+                setServer(tempServer);
+                setServerStatus("connecting");
             }} expand={"full"}>
                 Synchronize with server
             </AppButton>}
+            {serverStatus === "connected" && <AppChip color="favorite">Synchronizing data with {server}</AppChip>}
         </AppCard >
     </AppModal > : <AppButton onClick={() => {
         setShowSettings(true);
--- src/hooks/useAppSettings.ts
+++ src/hooks/useAppSettings.ts
@@ -1,45 +1,59 @@
 import create from "zustand";
 import { set as idbSet, get } from "idb-keyval"
 import { AppConfig } from "../util";
+export type ServerStatus = "unknown" | "connected" | "needs-permission" | "connecting" | "error";
 export interface AppSettingsProperties {
     encryption?: string,
     darkMode: boolean,
     server?: string,
     initialized: boolean,
+    authorized: boolean,
+    serverStatus: ServerStatus
 }
 export interface AppSettingCache extends AppSettingsProperties {
+    setServerStatus: (serverStatus: ServerStatus) => void
     setServer: (server: string) => void
     initialize: (config: AppConfig) => void
     setDarkMode: (isDark: boolean) => void
+    setAuthorized: (authorized: boolean) => void
     serialize: () => void
 }
 /**
 *  Application Cache status
 */
-export const useAppSettings = create<AppSettingCache>((set, cache) => ({
+export const useAppSettings = create<AppSettingCache>((set, settings) => ({
+    serverStatus: "unknown",
     darkMode: true,
     initialized: false,
+    authorized: false,
     initialize: async (appConfig) => {
-        const savedCache = await get("atomic-settings")
+        const savedSettings = await get("atomic-settings")
         const cacheFields = {
-            ...JSON.parse(savedCache)
+            ...JSON.parse(savedSettings)
         } as AppSettingsProperties
         // Combine serialized settings and app-config
         const { encryption, darkMode, server } = { ...appConfig, ...cacheFields };
         set({ encryption, darkMode, server, initialized: true })
-        cache().serialize();
+        settings().serialize();
     },
     serialize: () => {
-        const { server, darkMode, encryption } = cache()
+        const { server, darkMode, encryption } = settings()
         idbSet("atomic-settings", JSON.stringify(({ server, darkMode, encryption })))
+    },
+    setAuthorized: (authorized) => {
+        set({ authorized })
+        settings().serialize();
     },
     setServer: (server) => {
         set({ server })
-        cache().serialize();
+        settings().serialize();
+    },
+    setServerStatus: (serverStatus) => {
+        set({ serverStatus })
     },
     setDarkMode: (darkMode) => {
         set({ darkMode })
-        cache().serialize();
+        settings().serialize();
     }
 }));
 
