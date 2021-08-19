3b07243a10fe2c83230c5363d9a8e5fc8ef48c2c
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-08-19 13:36:41 -0600

    Update AppTabs.js

--- dist/components/AppTabs.js
+++ dist/components/AppTabs.js
@@ -1,6 +1,6 @@
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-import { IonRouterOutlet, IonTabs } from '@ionic/react';
+import { IonRouterOutlet, IonTabBar, IonTabs } from '@ionic/react';
 import { AppBadge, AppChip, AppIcon, AppLabel } from 'atomic';
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
@@ -21,7 +21,7 @@ const AppTabs = props => {
       setCurrentTab(event.detail.tab);
     },
     onIonTabsDidChange: props.onTabsDidChange
-  }, props), props.tabs.map(tab => /*#__PURE__*/React.createElement(AppTabButton, {
+  }, props), /*#__PURE__*/React.createElement(IonTabBar, null, props.tabs.map(tab => /*#__PURE__*/React.createElement(AppTabButton, {
     style: currentTab === tab.path ? {
       "--color": "var(--ion-color-" + props.selectedColor || "primary"
     } : {},
@@ -30,7 +30,7 @@ const AppTabs = props => {
     icon: tab.icon
   }), /*#__PURE__*/React.createElement(AppLabel, null, tab.title), tab.notifications && /*#__PURE__*/React.createElement(AppBadge, {
     color: tab.notificationColor
-  }, tab.notifications))), /*#__PURE__*/React.createElement(IonRouterOutlet, null, /*#__PURE__*/React.createElement(Route, {
+  }, tab.notifications)))), /*#__PURE__*/React.createElement(IonRouterOutlet, null, /*#__PURE__*/React.createElement(Route, {
     path: "*",
     component: () => /*#__PURE__*/React.createElement(React.Fragment, null, tabs[currentTab].component || /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(AppChip, {
       color: "danger"
