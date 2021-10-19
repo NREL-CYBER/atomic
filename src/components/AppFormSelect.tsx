ddbc9b0f7ec06c94d643f93ec36bffabbf7df1d9
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-10-19 15:52:41 -0600

    Update AppFormSelect.tsx

--- src/components/AppFormSelect.tsx
+++ src/components/AppFormSelect.tsx
@@ -25,34 +25,11 @@ export interface formSelectInputProps {
 const AppFormSelect = (props: formSelectInputProps) => {
     const { propertyInfo, instanceRef, onChange, property, required } = props;
     const [errors, setErrors] = useState<string[] | undefined>(undefined);
-    const [inputStatus, setInputStatus] = useState<InputStatus>(typeof instanceRef.current === "undefined" ? "empty" : "valid");
+    const [inputStatus, setInputStatus] = useState<InputStatus>(typeof instanceRef.current[property] === "undefined" ? "empty" : "valid");
     let instanceValue = instanceRef.current && (instanceRef.current as any)[property];
-    if (isUndefined(instanceValue)) {
-        instanceRef.current[property] = "";
-    }
     const [value, setValue] = useState<string>(instanceValue);
     const propertyFormattedName = prettyTitle(propertyInfo.title ? propertyInfo.title : property);
-
     const statusColor = inputStatusColorMap[inputStatus];
-
-    const updateSelection = useCallback((val: string) => {
-        if (val === "" || typeof val === "undefined" || val === null) {
-            setInputStatus("empty");
-            return;
-        }
-        onChange(property, val).then(([validationStatus, validationErrors]) => {
-            setInputStatus(validationStatus);
-            setErrors(validationErrors);
-            setValue(value);
-        });
-    }, [onChange, property, value]);
-
-    useEffect(() => {
-        updateSelection(value);
-    }, [updateSelection, value])
-
-
-
     return <>
         <AppItem>
             <AppFormLabel required={required}
@@ -60,8 +37,12 @@ const AppFormSelect = (props: formSelectInputProps) => {
                 color={statusColor} />
             <AppCol>
 
-                <AppSelect interface="popover" value={value} placeholder={propertyFormattedName} onSelectionChange={(selection) => {
-                    setValue(selection);
+                <AppSelect interface="popover" value={value} placeholder={propertyFormattedName} onSelectionChange={(val) => {
+                    setValue(val);
+                    onChange(property, val).then(([validationStatus, validationErrors]) => {
+                        setInputStatus(validationStatus);
+                        setErrors(validationErrors);
+                    });
                 }}>
                     {propertyInfo.enum.map((enumValue: string) => < AppSelectOption key={enumValue} value={enumValue} children={titleCase(enumValue)} />)}
                 </AppSelect>
