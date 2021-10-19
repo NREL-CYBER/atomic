b6a8524cd69a3827577dc63a4d2f499bf21071d6
Paul Wand <66976845+SilentEsper@users.noreply.github.com>
2021-10-19 15:54:12 -0600

    Update AppFormSelect.js

--- dist/components/AppFormSelect.js
+++ dist/components/AppFormSelect.js
@@ -1,6 +1,6 @@
 import { AppCol } from 'atomic';
-import React, { useCallback, useEffect, useState } from 'react';
-import { isUndefined, prettyTitle } from '../util';
+import React, { useState } from 'react';
+import { prettyTitle } from '../util';
 import titleCase from '../util/titleCase';
 import { inputStatusColorMap } from './AppFormInput';
 import AppItem from './AppItem';
@@ -21,31 +21,11 @@ const AppFormSelect = props => {
     required
   } = props;
   const [errors, setErrors] = useState(undefined);
-  const [inputStatus, setInputStatus] = useState(typeof instanceRef.current === "undefined" ? "empty" : "valid");
+  const [inputStatus, setInputStatus] = useState(typeof instanceRef.current[property] === "undefined" ? "empty" : "valid");
   let instanceValue = instanceRef.current && instanceRef.current[property];
-
-  if (isUndefined(instanceValue)) {
-    instanceRef.current[property] = "";
-  }
-
   const [value, setValue] = useState(instanceValue);
   const propertyFormattedName = prettyTitle(propertyInfo.title ? propertyInfo.title : property);
   const statusColor = inputStatusColorMap[inputStatus];
-  const updateSelection = useCallback(val => {
-    if (val === "" || typeof val === "undefined" || val === null) {
-      setInputStatus("empty");
-      return;
-    }
-
-    onChange(property, val).then(([validationStatus, validationErrors]) => {
-      setInputStatus(validationStatus);
-      setErrors(validationErrors);
-      setValue(value);
-    });
-  }, [onChange, property, value]);
-  useEffect(() => {
-    updateSelection(value);
-  }, [updateSelection, value]);
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(AppItem, null, /*#__PURE__*/React.createElement(AppFormLabel, {
     required: required,
     name: propertyFormattedName,
@@ -54,8 +34,12 @@ const AppFormSelect = props => {
     interface: "popover",
     value: value,
     placeholder: propertyFormattedName,
-    onSelectionChange: selection => {
-      setValue(selection);
+    onSelectionChange: val => {
+      setValue(val);
+      onChange(property, val).then(([validationStatus, validationErrors]) => {
+        setInputStatus(validationStatus);
+        setErrors(validationErrors);
+      });
     }
   }, propertyInfo.enum.map(enumValue => /*#__PURE__*/React.createElement(AppSelectOption, {
     key: enumValue,
